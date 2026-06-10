ce4bad78f0978d6abf55557a79927068107f25af
erankit011 <erankit011@gmail.com>
2026-06-10 22:17:17 +0530

    name added

--- src/repositories/implementations/mongoAITestRepository.js
+++ src/repositories/implementations/mongoAITestRepository.js
@@ -45,7 +45,7 @@ class MongoAITestRepository extends IAITestRepository {
           },
         },
       ]);
-
+// hi aman
       if (aitests?.length === 0) {
         throw new Error("AITest not found");
       }
